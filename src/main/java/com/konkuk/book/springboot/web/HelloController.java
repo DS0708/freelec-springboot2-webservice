064a3c5280be7e401c65e6cdc2f4db03e439b86b
DS0708 <tlsehdtjd070@naver.com>
2023-10-24 07:00:47 +0900

    Add Unit Test of TDD

--- src/main/java/com/konkuk/book/springboot/web/HelloController.java
+++ src/main/java/com/konkuk/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.konkuk.book.springboot.web;
 
+import com.konkuk.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줍니다.
@@ -13,4 +15,9 @@ public class HelloController {
     public String hello(){
         return "hello";
     } // /hello로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 되었습니다.
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
 }
--- src/test/java/com/konkuk/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/konkuk/book/springboot/web/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.konkuk.book.springboot;
+package com.konkuk.book.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,11 +23,24 @@ public class HelloControllerTest {
 
     @Test
     public void return_hello() throws Exception {
-        String hello = "hello23";
+        String hello = "hello";
 
         mvc.perform(get("/hello"))  //MockMvc를 통해 /hello 주소로 HTTP GET 요청을 합니다
                 .andExpect(status().isOk())    //200 이면 성공
                 .andExpect(content().string(hello));    // 응답 본문의 리턴 값이 hello와 일치하면 성공
+    }
+
+    @Test
+    public void return_HelloDto() throws Exception{
+        String name = "hello";
+        int amount =  1000;
+        String name2 = "hello";
+        int amount2 =  1000;
+
+        mvc.perform(get("/hello/dto").param("name",name2).param("amount",String.valueOf(amount2))) //get의 파라미터 설정
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 }
