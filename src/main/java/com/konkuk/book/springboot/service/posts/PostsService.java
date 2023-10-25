cce8a121df577d25a66131785d8435ea192ee1d4
DS0708 <tlsehdtjd070@naver.com>
2023-10-26 07:05:29 +0900

    update기능 TDD 작성

--- src/main/java/com/konkuk/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/konkuk/book/springboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package com.konkuk.book.springboot.service.posts;
 
 
+import com.konkuk.book.springboot.domain.posts.Posts;
 import com.konkuk.book.springboot.domain.posts.PostsRepository;
+import com.konkuk.book.springboot.web.dto.PostsResponseDto;
 import com.konkuk.book.springboot.web.dto.PostsSaveRequestDto;
+import com.konkuk.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        //update 부분에서 따로 쿼리를 날리지 않는 이유 = dirty checking (https://jojoldu.tistory.com/415)
+
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 
 
