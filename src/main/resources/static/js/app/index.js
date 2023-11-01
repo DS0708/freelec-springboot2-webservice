cd6a4ae46b7f3a7fce9d32ac4f3a2812659a1726
DS0708 <tlsehdtjd070@naver.com>
2023-11-02 01:06:17 +0900

    Create js for button event listener

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,33 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function (){
+            _this.save();
+        });
+    },
+    save : function (){
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){ //AJAX 요청이 성공했을 때 실행되는 콜백 함수
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).failIfMajorPerformanceCaveat(function (){ //AJAX 요청이 실패하거나 주요한 성능 문제가 발생했을때 실행되는 콜백 함수
+            alert(JSON.stringify(error));
+        });
+    }
+};
+/*
+* 다른 JS와 init, save이름이 겹칠 수 있으므로 index.js만의 Scope를 만들어서 사용
+* */
+
+main.init() //이벤트 리스너 설정
