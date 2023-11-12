26feddb2e2f22157e2cb877d45598174955f2f19
DS0708 <tlsehdtjd070@naver.com>
2023-11-12 21:13:06 +0900

    네이버 로그인 기능 추가

--- src/main/java/com/konkuk/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/konkuk/book/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.konkuk.book.springboot.config.auth.dto;
 import com.konkuk.book.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {  //SessionUser에는 인증된 사용자 정보만 필요함
+public class SessionUser implements Serializable {  //SessionUser에는 인증된 사용자 정보만 필요함
     private String name;
     private String email;
     private String picture;
