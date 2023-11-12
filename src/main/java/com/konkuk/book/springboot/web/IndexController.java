5191da830db10be1902c407c0e5cfe6a1acde5a1
DS0708 <tlsehdtjd070@naver.com>
2023-11-12 19:48:19 +0900

    @LoginUser 어노테이션 추가

--- src/main/java/com/konkuk/book/springboot/web/IndexController.java
+++ src/main/java/com/konkuk/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.konkuk.book.springboot.web;
 
+import com.konkuk.book.springboot.config.auth.LoginUser;
 import com.konkuk.book.springboot.config.auth.dto.SessionUser;
 import com.konkuk.book.springboot.service.posts.PostsService;
 import com.konkuk.book.springboot.web.dto.PostsResponseDto;
@@ -19,14 +20,15 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){   //Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+    public String index(Model model, @LoginUser SessionUser user){   //Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
 
         model.addAttribute("posts",postsService.findAllDesc());
         //postsService.findAllDesc()의 결과를 posts이름으로 index.mustache로 전달
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        //앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
-        // 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        //앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+//        // 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+
         if(user != null){ //세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
             model.addAttribute("userName", user.getName());
         }
