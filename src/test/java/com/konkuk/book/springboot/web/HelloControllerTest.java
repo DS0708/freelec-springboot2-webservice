35bb3a6cc530b9c5e762a0f272ee0ff836f14d4b
DS0708 <tlsehdtjd070@naver.com>
2023-11-18 02:19:31 +0900

    gradle 버전 업그레이드 4.10.2 -> 6.7.1

--- src/test/java/com/konkuk/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/konkuk/book/springboot/web/HelloControllerTest.java
@@ -1,14 +1,14 @@
 package com.konkuk.book.springboot.web;
 
 import com.konkuk.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class,
         excludeFilters = { //스캔 대상에서 SecurityConfig를 제거
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
@@ -29,7 +29,7 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc; //이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있습니다.
 
-    @WithMockUser(roles="USER")
+    @WithMockUser(roles = "USER")
     @Test
     public void return_hello() throws Exception {
         String hello = "hello";
@@ -39,15 +39,15 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));    // 응답 본문의 리턴 값이 hello와 일치하면 성공
     }
 
-    @WithMockUser(roles="USER")
+    @WithMockUser(roles = "USER")
     @Test
-    public void return_HelloDto() throws Exception{
+    public void return_HelloDto() throws Exception {
         String name = "hello";
-        int amount =  1000;
+        int amount = 1000;
         String name2 = "hello";
-        int amount2 =  1000;
+        int amount2 = 1000;
 
-        mvc.perform(get("/hello/dto").param("name",name2).param("amount",String.valueOf(amount2))) //get의 파라미터 설정
+        mvc.perform(get("/hello/dto").param("name", name2).param("amount", String.valueOf(amount2))) //get의 파라미터 설정
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is(name)))
                 .andExpect(jsonPath("$.amount", is(amount)));
--- src/test/java/com/konkuk/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/konkuk/book/springboot/web/PostsApiControllerTest.java
@@ -6,17 +6,17 @@ import com.konkuk.book.springboot.domain.posts.Posts;
 import com.konkuk.book.springboot.domain.posts.PostsRepository;
 import com.konkuk.book.springboot.web.dto.PostsSaveRequestDto;
 import com.konkuk.book.springboot.web.dto.PostsUpdateRequestDto;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,7 +29,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //@WebMvcTest는 JPA기능이 작동하지 않기 때문에 @SpringBootTest와 TestRestTemplate를 함께사용, 랜덤포트 실행
 public class PostsApiControllerTest {
 
@@ -38,7 +38,7 @@ public class PostsApiControllerTest {
 
     private MockMvc mvc;
 
-    @Before
+    @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
@@ -55,7 +55,7 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
-    @After
+    @AfterEach
     public void tearDown() throws Exception{
         postsRepository.deleteAll();
     }
